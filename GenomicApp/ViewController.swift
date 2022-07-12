921036177486a183a0e28424379b8248c46d1f89
Federico Bustos Fierro <federico@muuvlabs.com>
2022-07-11 21:41:57 -0300

    Initial Commit

---
+++ GenomicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenomicApp
+//
+//  Created by Lucas on 11/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
